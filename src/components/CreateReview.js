371947e82cd891359892b2bd15dee4adcef0ccd0
yoyo <hsjejxys@gmail.com>
2024-05-02 18:15:18 +0900

    投稿内容をDBと連携

--- src/components/CreateReview.js
+++ src/components/CreateReview.js
@@ -1,15 +1,44 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "../style/App.css";
 
 export const CreateReview = () => {
   const [image, setImage] = useState(null);
+  const [title, setTitle] = useState("");
+  const [comment, setComment] = useState("");
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file && file.type.substr(0, 5) === "image") {
-      setImage(URL.createObjectURL(file));
+      setImage(file);
     } else {
       setImage(null);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // フォームのデフォルト送信を防ぐ
+
+    // 画像を送信
+    if (image) {
+      const formData = new FormData();
+      formData.append("image", image);
+      try {
+        await axios.post("http://localhost:8080/books/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+      } catch (error) {
+        console.error("Error uploading image", error);
+      }
+    }
+
+    // タイトルとコメントを送信
+    try {
+      await axios.post("http://localhost:8080/books", { title, comment });
+    } catch (error) {
+      console.error("Error posting review", error);
     }
   };
 
@@ -19,18 +48,27 @@ export const CreateReview = () => {
       <hr />
       <div className="app-container">
         <div className="form-container">
-          <form>
+          <form onSubmit={handleSubmit}>
             <ul>
               <li>
                 <div className="review-image">表紙</div>
                 <input type="file" onChange={handleImageChange} />
                 {image && (
-                  <img src={image} alt="Preview" style={{ width: "30%" }} />
+                  <img
+                    src={URL.createObjectURL(image)}
+                    alt="Preview"
+                    style={{ width: "30%" }}
+                  />
                 )}
               </li>
               <li>
                 <div className="review-title">タイトル</div>
-                <input type="text" className="review-title-container" />
+                <input
+                  type="text"
+                  className="review-title-container"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
               </li>
               <li>
                 <div className="review-comment">コメント</div>
@@ -38,6 +76,8 @@ export const CreateReview = () => {
                   cols="100"
                   rows="10"
                   className="review-comment-container"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
                 ></textarea>
               </li>
             </ul>
