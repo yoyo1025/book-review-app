2c8aa9dd4f523a43d378d5c3e72eef9a84f67022
yoyo <hsjejxys@gmail.com>
2024-06-22 17:37:31 +0900

    Top画面に画像を表示

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,8 +1,18 @@
 import Navi from "./Navi";
-import { useEffect, useState } from "react";
+import code from "./../images/code.jpg"
+import robot from "./../images/robot.jpg"
+
 
 export const Top = () => {
-  return <Navi />;
+  return (
+    <>
+      <Navi />
+      <div className="top-pictures">
+        <img src={code} className="image-code"/>
+        <img src={robot} className="image-robot"/>
+      </div>
+    </>
+);
 };
 
 export default Top;
